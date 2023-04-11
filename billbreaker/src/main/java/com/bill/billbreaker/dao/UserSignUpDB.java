c54d50a44b9dab5408fbeb9adb1952b237839e1d
noolisainathareddy <nooli.sainathareddy@gmail.com>
2023-04-10 22:41:56 -0500

    Added API documentation

--- billbreaker/src/main/java/com/bill/billbreaker/dao/UserSignUpDB.java
+++ billbreaker/src/main/java/com/bill/billbreaker/dao/UserSignUpDB.java
@@ -1,12 +1,13 @@
 package com.bill.billbreaker.dao;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Data
