f7cbb6a7dc7f2e4d8c467e81d73c144eeecc9b85
noolisainathareddy <nooli.sainathareddy@gmail.com>
2023-04-14 15:39:50 -0500

    Added Logging and Docker file

--- billbreaker/src/main/java/com/bill/billbreaker/serviceImpl/UserSignUpServiceImpl.java
+++ billbreaker/src/main/java/com/bill/billbreaker/serviceImpl/UserSignUpServiceImpl.java
@@ -4,12 +4,14 @@ import com.bill.billbreaker.dao.UserSignUpDB;
 import com.bill.billbreaker.dao.UserUniqueKey;
 import com.bill.billbreaker.repository.UserSignUpRepository;
 import com.bill.billbreaker.service.UserSignUpService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Log4j2
 public class UserSignUpServiceImpl implements UserSignUpService {
 
     @Autowired
@@ -19,14 +21,14 @@ public class UserSignUpServiceImpl implements UserSignUpService {
     @Override
     public String UserSigning(UserSignUpDB userSignUpDB) {
         Optional<UserSignUpDB> value = userSignUpRepository.findById(userSignUpDB.getUserUniqueKey());
-
+        log.info("Started registering in DB");
         if(value.isPresent()){
             return "Email and Mobile Number already used";
         }
-        else{
+        else {
             userSignUpRepository.save(userSignUpDB);
             return "SignedUp successfully";
-        }
 
+        }
     }
 }
